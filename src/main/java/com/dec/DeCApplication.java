e2f95d2d29d199ea0889ee9e0e0eb981c6533ff8
xengom <xengom@gmail.com>
2022-06-15 22:24:39 +0900

    .

---
+++ src/main/java/com/dec/DeCApplication.java
@@ -0,0 +1,13 @@
+package com.dec;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DeCApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DeCApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/dec/DeCApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dec;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DeCApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
